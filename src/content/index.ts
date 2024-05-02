3ae164292bcc9245a2b0834974a22973a21d7da1
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-02 16:36:23 +0200

    clean code

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,16 +1,17 @@
-import { setBrowserDesign } from "./setBrowserDesign";
-import { CBrowserManagerSidebar } from "./browser-manager-sidebar";
+import {setBrowserDesign} from "./setBrowserDesign";
+import {CBrowserManagerSidebar} from "./browser-manager-sidebar";
 
 //console.log("hi!");
 
 //@ts-expect-error ii
 SessionStore.promiseInitialized.then(() => {
-  setBrowserDesign();
-  //createWebpanel("tmp", "https://manatoki332.net/");
-  //console.log(document.getElementById("tmp"));
-  //window.gBrowserManagerSidebar = CBrowserManagerSidebar.getInstance();
-  import("./testButton");
-  import("./browser-sidebar/index");
+  setBrowserDesign().then(() => {
+    //createWebpanel("tmp", "https://manatoki332.net/");
+    //console.log(document.getElementById("tmp"));
+    //window.gBrowserManagerSidebar = CBrowserManagerSidebar.getInstance();
+    import("./testButton");
+    import("./browser-sidebar/index");
+  });
 });
 
 //Services.obs.addObserver(setBrowserDesign, "browser-window-before-show");
